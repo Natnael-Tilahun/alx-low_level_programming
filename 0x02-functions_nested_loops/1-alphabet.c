a46f3cba65e11c93d31d94f941bf09c5b7428cdc
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-03-15 06:11:32 -0700

    Solve task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,10 @@ int main(void)
 print_alphabet();
 return (0);
 }
-
-int print_alphabet(void){
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
 }
