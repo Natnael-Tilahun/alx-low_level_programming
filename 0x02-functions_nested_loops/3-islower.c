2e285053df48982fd306188e025b4f5cb58b32c8
Natnael Tilahun <69369304+Natnael-Tilahun@users.noreply.github.com>
2022-03-16 16:24:50 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
 * islower - checks lowercase 
 * @c: contains value to be checked
 * Return 0: Always 0.
 */
-=======
- * _islower - Short description, single line
- * @c: contains value to be compared
- * Return: Always .
- */
->>>>>>> 826e23a0db881900954759bc4a6a6fccf5460651
 
 int _islower(int c)
 {
