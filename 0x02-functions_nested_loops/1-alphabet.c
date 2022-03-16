6b1ffd47759094d8c902120bc0b6e1c10bd67506
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-03-15 23:29:10 -0700

    Solve task 2 error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "1-alphabet.h"
+#include "alphabet.h"
 void print_alphabet(void)
 {
 char letter;
--- 0x02-functions_nested_loops/1-alphabet.h
+++
@@ -1,6 +0,0 @@
-#ifndef 1-alphabet.h
-#define 1-alphabet.h
-
-void print_alphabet(void);
-
-#endif
