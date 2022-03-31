4d7fa90f3ceffedb9e163fe37b047d048de9f5ca
Natnael Tilahun <69369304+Natnael-Tilahun@users.noreply.github.com>
2022-03-31 09:13:57 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,16 @@
 #include "main.h"
 /**
- * _strcmp - Compares pointers to two strings.
- * @s1: A pointer to the first string to be compared.
- * @s2: A pointer to the second string to be compared.
- *
- * Return: If str1 < str2, the negative difference of
- * the first unmatched characters.
- *         If s1 == s2, 0.
- *         If s1 > s2, the positive difference of
- *         the first unmatched characters.
- */
-int _strcmp(char *s1, char *s2)
+* _puts - print a string followed by a new line
+*@str: string to print
+*Return: Nothing
+*/
+void _puts(char *str)
 {
-while ((*s1 && *s2) && (*s1 == *s2))
-{
-s1++;
-s2++;
-}
-return (*s1 - *s2);
+	int i;
+
+	for (i = 0; *(str + i) != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
