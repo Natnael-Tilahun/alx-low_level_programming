98b6dd8b4e8a559028f9a665927c5d054a1e4afa
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-14 06:58:24 -0700

    Solve task 3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - returns a pointer of the concatination of two strings
+ * @s1 : first string
+ * @s2 : second string
+ *
+ *
+ * Return : pointer of array of chars
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *str;
+unsigned int i, j;
+  
+}
