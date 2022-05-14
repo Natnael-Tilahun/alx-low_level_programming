854e75884910b1dc021364e32d4d5f6ce6f2165c
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-14 06:18:06 -0700

    Solve task 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,16 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
-\**
-* _strdup - returns a pointer to a newly allocated space in memory
-* @str : string
-*
-* Return : pointer of an array of chars
-*
-*
-*
-*
-*/
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str : string
+ *
+ * Return: pointer of an array of chars
+ *
+ */
 
 char *_strdup(char *str)
 {
