e55d96fa1818599202db86f70daa955525dd9581
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-17 05:59:43 -0700

    Solve advanced task 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,7 +5,7 @@
  * _realloc - re allocate new memory
  * @old_size: size of the previous memory
  * @new_size: size of newly created memory
- *
+ * @ptr: pointer of previously allocated memory
  *
  *
  * Return: pointer of reallocated memory
@@ -13,30 +13,30 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  char *p;
-	unsigned int i, max = new_size;
-	char *oldp = ptr;
+char *p;
+unsigned int i, max = new_size;
+char *oldp = ptr;
 
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		return (p);
-	}
-	else if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	else if (new_size == old_size)
-		return (ptr);
+if (ptr == NULL)
+{
+p = malloc(new_size);
+return (p);
+}
+else if (new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+else if (new_size == old_size)
+return (ptr);
 
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	if (new_size > old_size)
-		max = old_size;
-	for (i = 0; i < max; i++)
-		p[i] = oldp[i];
-	free(ptr);
-	return (p);
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+if (new_size > old_size)
+max = old_size;
+for (i = 0; i < max; i++)
+p[i] = oldp[i];
+free(ptr);
+return (p);
 }
