b9e2cc41fcd8a013ce0e7e719533f8ab55e67fc5
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-20 12:51:44 -0700

    Solve task 1 on structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure that stores information about dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Discription: the struct which is called dog that stores the name, age
+ * and owner of the dog
+ */
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+dog_t *new(char *name, float age, chat *owner);
 
 #endif
