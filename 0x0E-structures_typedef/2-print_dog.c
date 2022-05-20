6d5e8f3e2e6165699598f51287361d64cf241b43
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-20 13:33:27 -0700

    Solve task 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,14 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * print_dog - prints struct variables
+ * @d: struct dog
+ * Retrun: nothing
+ */
+
 void print_dog(struct dog *d)
 {
 if (d)
 {
-if (d->name == NULL)
+if (d->name == NULL || d->owner == NULL)
 {
-printf("Name: nil");
+printf("Name: (nil)\n");
 }
-printf("Name: %s\n Age: %.f\n Owner: %s", d->name, d->age, d->owner);
+printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
 }
