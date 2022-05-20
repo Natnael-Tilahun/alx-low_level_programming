1e118cc3d75a4ad98d63b502f95abbe99047549a
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-20 13:40:17 -0700

    Solve task 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,13 @@ if (d->name == NULL)
 {
 printf("Name: (nil)\n");
 }
+else if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
 printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
+}
 }
 }
