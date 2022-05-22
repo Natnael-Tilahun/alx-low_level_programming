972e3e6909ca1a317c77ceb13544ee954fba722f
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-05-22 07:58:36 -0700

    Solve task 1 on pointer function project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to a char
+ * @f: pointer to function that accepts char pointer and return nothing
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void(*f) (char *))
+{
+}
