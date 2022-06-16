e3c1abdd05d5b33c1fbb5ff7fc1b215d3cb7caa5
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-06-16 13:15:30 -0700

    Solve advanced taskss

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$PWD/libruin.so
+wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/m\
+     aster/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD =/tmp/nrandom.so
