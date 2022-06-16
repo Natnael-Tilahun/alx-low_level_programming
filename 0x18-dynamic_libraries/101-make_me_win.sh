2a8981a219f1ba8596718b3b6dbc54e1dd700d62
Natnael Tilahun <69369304+Natnael-Tilahun@users.noreply.github.com>
2022-06-16 22:46:17 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/svelezg/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
-export LD_PRELOAD=../inject.so
+gcc -c -Wall -Werror -fPIC *.c
+gcc -shared -o liball.so *.o
